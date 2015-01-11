2cba039dcf25b0fe7183943f39b5a45b5d76edbf
Aaron Landy <aaron@thecompassmag.com>
2015-01-11 13:49:36 -0800

    radar

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use("/", express.static(__dirname));
 
 app.get('/', function(req, res){
+  res.sendfile('home.html');
+});
+app.get('/radar', function(req, res){
   res.sendfile('index.html');
 });
 
