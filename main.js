59dd4ae418c5ad34077c8b8bebb91f9466f45f31
Andrew Breckenridge <asbreckenridge@me.com>
2015-01-10 21:16:42 -0800

    both circles work in the DOM

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
-var position = [27.1959739, 78.02423269999997];
+var position = [37.688613, -122.468703];
+var map1 = "";
+var map2 = "";
 
-function showGoogleMaps() {
+function loadBgMap() {
+  var latLng = new google.maps.LatLng(position[0], position[1]);
 
+  var mapOptions = {
+    zoom: 16, // initialize zoom level - the max value is 21
+    streetViewControl: false, // hide the yellow Street View pegman
+    scaleControl: true, // allow users to zoom the Google Map
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: latLng
+  };
+
+  var map1 = new google.maps.Map(document.getElementById('bg-map'), mapOptions);
+  return map1;
+}
+
+function loadFgMap() {
     var latLng = new google.maps.LatLng(position[0], position[1]);
 
     var mapOptions = {
-        zoom: 16, // initialize zoom level - the max value is 21
-        streetViewControl: false, // hide the yellow Street View pegman
-        scaleControl: true, // allow users to zoom the Google Map
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: latLng
+      zoom: 21, // initialize zoom level - the max value is 21
+      streetViewControl: false, // hide the yellow Street View pegman
+      scaleControl: false, // allow users to zoom the Google Map
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: latLng
     };
 
-    map = new google.maps.Map(document.getElementById('bg-map'), mapOptions);
-
-    // Show the default red marker at the location
-    marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP
-    });
+    map2 = new google.maps.Map(document.getElementById('fg-map'), mapOptions);
+    return map2;
 }
 
-google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+document.onload = function() {
+  map1 = loadBgMap();
+  map2 = loadFgMap();
+}
+
+google.maps.event.addDomListener(window, 'load', loadBgMap);
+google.maps.event.addDomListener(window, 'load', loadFgMap);
+//
+// console.log(map1);
+// console.log(map2);
