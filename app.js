99da07670b935bd03190b898b45e3f8334127d6d
Aaron Landy <aaron@thecompassmag.com>
2015-01-11 11:56:47 -0800

    port

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('port', (process.env.PORT || 5000))
+
 app.use("/", express.static(__dirname));
 
 app.get('/', function(req, res){
@@ -13,6 +15,6 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(5000, function(){
-  console.log('listening on 5000');
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 });
